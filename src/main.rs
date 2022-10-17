3139e62445f4aa8d26455f43d9f81263a65012d1
Adryzz <adryzz@vignetteapp.org>
2022-10-18 00:37:49 +0200

    fix code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use ctru::prelude::*;
 use ctru::services::hid::TouchPosition;
 use std::io::Write;
-use std::net::{Shutdown, TcpListener, Ipv4Addr, TcpStream, ToSocketAddrs};
-use std::primitive;
-use std::time::Duration;
+use std::net::{Ipv4Addr, Shutdown, TcpListener, TcpStream};
 
 fn main() {
     ctru::init();
@@ -37,8 +35,7 @@ fn main() {
 
         let touch: (u16, u16) = TouchPosition::get(&mut TouchPosition::default());
 
-        if (connected)
-        {
+        if connected {
             // Handle touch event
             let mut arr: [u8; 4] = [0; 4];
             arr[0] = touch.0 as u8;
@@ -51,14 +48,12 @@ fn main() {
             if let Some(stream) = &mut stream_or_none {
                 stream.write(&arr);
             } else {
-            println!("Disconnected from client.");
-            connected = false;
-            console.clear();
-            setup(address);
+                println!("Disconnected from client.");
+                connected = false;
+                console.clear();
+                setup(address);
             }
-        }
-        else 
-        {
+        } else {
             // Accept connection if available
             if let Ok((_socket, addr)) = server.accept() {
                 println!("Connected to {addr:?}");
@@ -68,23 +63,18 @@ fn main() {
             }
         }
 
-        
-        if (keys != last_keys)
-        {
+        if keys != last_keys {
             if keys.intersects(KeyPad::KEY_START) {
                 println!("Exiting...");
-                if (connected)
-                {
+                if connected {
                     if let Some(stream) = &stream_or_none {
                         stream.shutdown(Shutdown::Both);
                     }
                     connected = false;
                 }
                 break;
-            }
-            else if keys.intersects(KeyPad::KEY_B) {
-                if (connected)
-                {
+            } else if keys.intersects(KeyPad::KEY_B) {
+                if connected {
                     console.clear();
                     println!("Disconnecting from client...");
                     if let Some(stream) = &stream_or_none {
@@ -93,38 +83,44 @@ fn main() {
                     connected = false;
                     setup(address);
                 }
-            }
-            else if keys.intersects(KeyPad::KEY_X) && calibration_step == 0 {
+            } else if keys.intersects(KeyPad::KEY_X) && calibration_step == 0 {
                 println!("Display calibration");
-                println!("Press and hold the stylus on the top left corner of the screen, then press A.");
+                println!(
+                    "Press and hold the stylus on the top left corner of the screen, then press A."
+                );
                 calibration_step = 1;
-            }
-            else if keys.intersects(KeyPad::KEY_A) {
-                if calibration_step == 1
-                {
-                    println!("\u{001b}[32mTop left: ({}, {})\u{001b}[0m", touch.0, touch.1);
+            } else if keys.intersects(KeyPad::KEY_A) {
+                if calibration_step == 1 {
+                    println!(
+                        "\u{001b}[32mTop left: ({}, {})\u{001b}[0m",
+                        touch.0, touch.1
+                    );
                     println!("Now press and hold the stylus on the top right corner of the screen, then press A.");
                     calibration_step = 2;
-                }
-                else if calibration_step == 2 {
-                    println!("\u{001b}[32mTop right: ({}, {})\u{001b}[0m", touch.0, touch.1);
+                } else if calibration_step == 2 {
+                    println!(
+                        "\u{001b}[32mTop right: ({}, {})\u{001b}[0m",
+                        touch.0, touch.1
+                    );
                     println!("Now press and hold the stylus on the bottom left corner of the screen, then press A.");
                     calibration_step = 3;
-                }
-                else if calibration_step == 3 {
-                    println!("\u{001b}[32mBottom left: ({}, {})\u{001b}[0m", touch.0, touch.1);
+                } else if calibration_step == 3 {
+                    println!(
+                        "\u{001b}[32mBottom left: ({}, {})\u{001b}[0m",
+                        touch.0, touch.1
+                    );
                     println!("Now press and hold the stylus on the bottom right corner of the screen, then press A.");
                     calibration_step = 4;
-                }
-                else if calibration_step == 4 {
-                    println!("\u{001b}[32mBottom right: ({}, {})\u{001b}[0m", touch.0, touch.1);
+                } else if calibration_step == 4 {
+                    println!(
+                        "\u{001b}[32mBottom right: ({}, {})\u{001b}[0m",
+                        touch.0, touch.1
+                    );
                     println!("Calibration completed!");
                     calibration_step = 0;
                 }
             }
         }
-
-        
 
         last_keys = keys;
 
@@ -134,8 +130,7 @@ fn main() {
         gfx.wait_for_vblank();
     }
 
-    fn setup(address: Ipv4Addr)
-    {
+    fn setup(address: Ipv4Addr) {
         println!("ctr-tablet-rs v0.1.0 by Lena");
         println!("https://github.com/adryzz/ctr-tablet-rs");
         println!("IP: {address}:5000");
