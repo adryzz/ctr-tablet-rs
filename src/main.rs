8777f1470e9a37b2c0cc944897e21fa277b980fa
Adryzz <adryzz@vignetteapp.org>
2022-10-17 16:57:27 +0200

    start stuff

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use ctru::prelude::*;
+use ctru::services::hid::TouchPosition;
+use std::net::{Shutdown, TcpListener, Ipv4Addr};
+use std::time::Duration;
+
+fn main() {
+    ctru::init();
+    let apt = Apt::init().unwrap();
+    let hid = Hid::init().unwrap();
+    let gfx = Gfx::init().unwrap();
+    let soc = Soc::init().unwrap();
+    let console = Console::init(gfx.top_screen.borrow_mut());
+
+    let address = soc.host_address();
+
+    let server = TcpListener::bind("0.0.0.0:5000").unwrap();
+    server.set_nonblocking(true).unwrap();
+
+    while apt.main_loop() {
+        // Scan for user input on the current frame.
+        hid.scan_input();
+
+        // Get information about which keys were held down on this frame
+        let keys = hid.keys_held();
+
+        let touch: (u16, u16) = TouchPosition::get(&mut TouchPosition::default());
+
+        // Clear the screen
+        console.clear();
+
+        // We print these again because we just cleared the screen above
+        println!("IP: {address}:5000");
+
+        if keys.intersects(KeyPad::KEY_START) {
+            println!("Exiting...");
+            break;
+        }
+
+        // Flush and swap framebuffers
+        gfx.flush_buffers();
+        gfx.swap_buffers();
+        gfx.wait_for_vblank();
+    }
+}
